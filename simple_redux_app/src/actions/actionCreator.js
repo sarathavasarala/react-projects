ac323b2f0a096527b80c334a8865a42f1d31cd21
Sarath <sarathavasarala@gmail.com>
2016-10-15 09:09:25 +0530

    cleaning unused files

--- simple_redux_app/src/actions/actionCreator.js
+++
@@ -1,8 +0,0 @@
-let nextId = 0;
-export const addComment = (comment) => {
-  return {
-    type:'ADD_COMMENT',
-    id:nextId++,
-    comment:comment  
-  }
-}
--- simple_redux_app/src/reducers/commentReducer.js
+++
@@ -1,9 +0,0 @@
-const commentReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_COMMENT':
-      return [...state, {id: action.id, comment: action.comment}]
-    default:
-      return state
-  }
-}
-export default commentReducer
